6568e6c95cb52624068f1c8c8d93dbcf81588c43
Cardell Jordan <cardghat@yahoo.com>
2025-04-08 21:35:02 -0400

    cline was able to suggest fixes due to updates in Terraform versioning

--- main.tf
+++ main.tf
@@ -2,15 +2,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_launch_configuration" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   # Required when using a launch configuration with an auto scaling group.
@@ -21,10 +27,9 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
-
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -43,13 +48,14 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-resource "aws_lb_listner" "http" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
   port              = 80
   protocol          = "HTTP"
@@ -63,6 +69,17 @@ resource "aws_lb_listner" "http" {
       message_body = "404: page not found"
       status_code  = 404
     }
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -89,7 +106,7 @@ resource "aws_security_group" "alb" {
 resource "aws_lb" "example" {
   name               = "terraform-asg-example"
   load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
+  subnets            = data.aws_subnets.default.ids
   security_groups    = [aws_security_group.alb.id]
 }
 
@@ -127,6 +144,6 @@ resource "aws_lb_listener_rule" "asg" {
 }
 
 output "alb_dns_name" {
-  value       = aws_lb.example.alb_dns_name
+  value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
